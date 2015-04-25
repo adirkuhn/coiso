fd689547222fff543a85c5ab4f687afa5cfef87f
Adir Kuhn <adirkuhn@gmail.com>
2015-04-25 15:49:12 -0300

    python parse

--- read_log.py
+++ read_log.py
@@ -1,11 +1,22 @@
 from flask import Flask, request
+import time
+import datetime
 app = Flask(__name__)
 
 
+def parse_log(line, user):
+    if ("userid=" + user + "\n" in line):
+        str_date = line[20:40]
+        return int(time.mktime(datetime.datetime.strptime(str_date, "%d/%b/%Y:%H:%M:%S").timetuple()))
 
+    return False
 
 @app.route("/")
-def read_log():
+def index():
+    #get user
+    user = request.args.get("user", "user2")
+
+    #server list
     servers = (
         'server1/server.log',
         'server2/server.log',
@@ -13,17 +24,25 @@ def read_log():
         'server4/server.log'
     )
 
-    file = open('server1/server.log', 'rb')
+    #parse files
+    log_out = {}
+    keys = []
+    for log in servers:
+        log_file = open('server1/server.log', 'rb')
+        for line in log_file:
+            ts = parse_log(line, user)
+            if (ts != False):
+                keys.append(ts)
+                log_out[str(ts)] = line
 
-    log = [];
-    for line in file:
-        log.append(line)
+    #out
+    out = "<pre>"
+    keys.sort()
+    for k in keys:
+        out += log_out[str(k)] + "<br>"
 
-    print log
-
-    user = request.args.get("user", "user2")
-    return "Hello World " + user
+    return out
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
